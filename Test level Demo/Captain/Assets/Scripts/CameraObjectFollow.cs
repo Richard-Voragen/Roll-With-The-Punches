967e790895ef36b8c847a792f6930ae3e80beb4c
jasonkohl <156859837+jmkohl1998@users.noreply.github.com>
2024-02-29 13:11:39 -0800

    Test Level

---
+++ Test level Demo/Captain/Assets/Scripts/CameraObjectFollow.cs
@@ -0,0 +1,57 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraObjectFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float speed = 0.75f;
+
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        this.targetPosition = this.transform.position;
+    }
+    
+    void FixedUpdate()
+    {
+        if (this.target)
+        {
+            var from = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+            var to = new Vector3(this.target.transform.position.x, this.transform.position.y, this.transform.position.z);
+            transform.position = Vector3.Lerp(from, to, this.speed);
+        }
+    }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Obscura
+{
+    public class CameraObjectFollow : MonoBehaviour
+    {
+        private Camera managedCamera;
+        private LineRenderer cameraLineRenderer;
+        public GameObject target;
+
+        private void Awake()
+        {
+            managedCamera = gameObject.GetComponent<Camera>();
+            cameraLineRenderer = gameObject.GetComponent<LineRenderer>();
+        }
+
+        //Use the LateUpdate message to avoid setting the camera's position before
+        //GameObject locations are finalized.
+        void LateUpdate()
+        {
+            var targetPosition = this.target.transform.position;
+            var cameraPosition = managedCamera.transform.position;
+
+            cameraPosition = new Vector3(targetPosition.x, targetPosition.y, cameraPosition.z);
+
+            managedCamera.transform.position = cameraPosition;
+        }
+    }
+}
---
+++ Test level Demo/Captain/Assets/Scripts/upAndDown.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upAndDown : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speed, 0) *  Time.deltaTime;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hjer");
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Bumper")
+        {
+            speed = -speed;
+        }
+    }
+}
