b1b8173a3c14c5e257c9f74a76690e0e0034c806
Prive082 <prive082@gmail.com>
2024-03-11 04:07:12 -0700

    Movement Established, All Sprite Sheets added

--- RollingWithThePunches/Assets/PlayerController.cs
+++ RollingWithThePunches/Assets/PlayerController.cs
@@ -5,30 +5,55 @@ using Player.Command;
 
 public class PlayerController : MonoBehaviour
 {
-
     private IPlayerCommand right;
     private IPlayerCommand left;
+    private IPlayerCommand jump;
+
+    bool isJumping = false;
+
     // Start is called before the first frame update
     void Start()
     {
         this.right = ScriptableObject.CreateInstance<MoveCharacterRight>();
         this.left = ScriptableObject.CreateInstance<MoveCharacterLeft>();
+        this.jump = ScriptableObject.CreateInstance<MoveCharacterJump>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Horizontal") > 0.01)
+        // Horizontal Movement
+        if (Input.GetAxis("Horizontal") > 0.01)
         {
             this.right.Execute(this.gameObject);
         }
-        if(Input.GetAxis("Horizontal") < -0.01)
+        if (Input.GetAxis("Horizontal") < -0.01)
         {
             this.left.Execute(this.gameObject);
         }
 
-        
+        // Update Animator
         var animator = this.gameObject.GetComponent<Animator>();
-        animator.SetFloat("Velocity", Mathf.Abs(this.gameObject.GetComponent<Rigidbody2D>().velocity.x/5.0f));
+
+        if (Input.GetAxis("Vertical") > 0.01 && !isJumping)
+        {
+            this.jump.Execute(this.gameObject);
+            isJumping = true; // Set jumping flag to true
+            animator.SetBool("IsJumping", true); // Trigger jumping animation
+        }
+
+        animator.SetFloat("Velocity", Mathf.Abs(this.gameObject.GetComponent<Rigidbody2D>().velocity.x / 5.0f));
+    }
+
+    // Check if the player is grounded
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var animator = this.gameObject.GetComponent<Animator>();
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            animator.SetBool("IsJumping", false);
+            isJumping = false; // Reset jumping flag when grounded
+        }
     }
 }
+
