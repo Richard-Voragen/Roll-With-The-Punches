0363828b8b344c2a6ea2108d347368b7df1589bc
Jared Martinez <jaredmartinez52101@gmail.com>
2024-03-12 02:18:47 -0700

    Basic Fireball

---
+++ RollingWithThePunches/Assets/Scripts/Fireball.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed = 10f;
+    public Vector2 direction = Vector2.right; 
+    public float lifetime = 2f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime); 
+    }
+
+    void Update()
+    {
+        rb.velocity = direction.normalized * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- RollingWithThePunches/Assets/Scripts/Movement/PlayerController.cs
+++ RollingWithThePunches/Assets/Scripts/Movement/PlayerController.cs
@@ -9,17 +9,21 @@ public class PlayerController : MonoBehaviour
     private IPlayerCommand left;
     private IPlayerCommand jump;
 
-    bool isJumping = false;
+    private IPlayerCommand shoot;
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject fireballPrefab; 
+    public float fireballSpawnDistance = 1f;
+
+    bool isJumping = false;
+    
+        void Start()
     {
         this.right = ScriptableObject.CreateInstance<MoveCharacterRight>();
         this.left = ScriptableObject.CreateInstance<MoveCharacterLeft>();
         this.jump = ScriptableObject.CreateInstance<MoveCharacterJump>();
+        this.shoot = new ShootFireball(fireballPrefab, fireballSpawnDistance);
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Horizontal Movement
@@ -27,14 +31,13 @@ public class PlayerController : MonoBehaviour
         {
             this.right.Execute(this.gameObject);
         }
-        if (Input.GetAxis("Horizontal") < -0.01)
+        else if (Input.GetAxis("Horizontal") < -0.01)
         {
             this.left.Execute(this.gameObject);
         }
 
-        // Update Animator
+        // Jumping
         var animator = this.gameObject.GetComponent<Animator>();
-
         if (Input.GetAxis("Vertical") > 0.01 && !isJumping)
         {
             this.jump.Execute(this.gameObject);
@@ -42,10 +45,16 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("IsJumping", true); // Trigger jumping animation
         }
 
+        // Shooting
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            this.shoot.Execute(this.gameObject);
+        }
+
+        // Update Animator Velocity
         animator.SetFloat("Velocity", Mathf.Abs(this.gameObject.GetComponent<Rigidbody2D>().velocity.x / 5.0f));
     }
 
-    // Check if the player is grounded
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var animator = this.gameObject.GetComponent<Animator>();
@@ -55,5 +64,22 @@ public class PlayerController : MonoBehaviour
             isJumping = false; // Reset jumping flag when grounded
         }
     }
-}
 
+    private class ShootFireball : IPlayerCommand
+    {
+        private readonly GameObject fireballPrefab;
+        private readonly float spawnDistance;
+
+        public ShootFireball(GameObject prefab, float spawnDistance)
+        {
+            this.fireballPrefab = prefab;
+            this.spawnDistance = spawnDistance;
+        }
+
+        public void Execute(GameObject gameObject)
+        {
+            Vector3 spawnPosition = gameObject.transform.position + gameObject.transform.right * spawnDistance;
+            Instantiate(fireballPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
